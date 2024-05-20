b8d89173190c025241059c8fe615d605a3ce41a5
Netzer7 <jacobhastings7@gmail.com>
2024-05-20 13:19:26 -0700

    Create Course and Assignment Models

---
+++ models/assignments.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize')
+
+const sequelize = require('../sequelize')
+const Course = require('./course')
+
+const Assignment = sequelize.define('assignment', {
+    courseid: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false, references: {
+        model: Course,
+        key: 'id'
+    },
+},
+    title: { type: DataTypes.STRING, allowNull: false },
+    points: { type: DataTypes.INTEGER, allowNull: false },
+    due: { type: DataTypes.DATE, allowNull: false },
+});
+
+Course.hasMany(Assignment, { foreignKey: 'courseid' });
+Assignment.belongsTo(Course, { foreignKey: 'courseid' });
+
+
+module.exports = Assignment
