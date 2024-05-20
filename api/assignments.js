c1ef52529f43e35d6fd224c131d2807653215631
Netzer7 <jacobhastings7@gmail.com>
2024-05-20 14:02:06 -0700

    Create Assignment GET by ID endpoint

--- api/assignments.js
+++ api/assignments.js
@@ -7,13 +7,26 @@ const router = Router()
 /*
  * Routes below.
  */
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     try {
         console.log(req.body)
         const assignment = await Assignment.create(req.body)
         res.status(201).send(assignment)
     } catch (e) {
-        console.log("error", e)
+        next(e)
+    }
+})
+
+router.get('/:assignmentId', async function (req, res, next) {
+    const assignmentId = req.params.assignmentId
+    try {
+        const assignment = await Assignment.findByPk(assignmentId)
+        if (assignment) {
+            res.status(200).send(assignment)
+        } else {
+            next()
+        }
+    } catch (e) {
         next(e)
     }
 })
